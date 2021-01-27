b068d482b7753af42bd3e9551b569b7a3871978f
wiktorwieclaw <lonevixu@gmail.com>
2021-01-27 02:02:23 +0100

    refactored club page

--- public/views/club.php
+++ public/views/club.php
@@ -2,21 +2,27 @@
 <head>
     <?php include("head.php") ?>
     <script type="text/javascript" src="/public/js/join.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="/public/css/club.css">
     <title>PROFILE</title>
 </head>
 <body>
 <div class="base-container">
     <?php include("header.php") ?>
     <div class="content">
-        <h1><?= $club->getTitle() ?></h1>
-        <button id="join-button" value="<?= $club->getId() ?>">join</button>
-        <div class="member-container">
-            <?php foreach ($members as $member): ?>
-                <a class="member" href="/profile/<?= $member['id_users'] ?>">
-                    <h3><?= $member['name'] ?></h3>
-                    <p><?= $member['surname'] ?></p>
-                </a>
-            <?php endforeach; ?>
+        <div class="club-page-container">
+            <div class=".club-info">
+                <h1><?= $club->getTitle() ?></h1>
+                <button id="join-button" value="<?= $club->getId() ?>">join</button>
+            </div>
+            <div class="member-container">
+                <h3>Members</h3>
+                <?php foreach ($members as $member): ?>
+                    <a class="member" href="/profile/<?= $member['id_users'] ?>">
+                        <h3><?= $member['name'] ?></h3>
+                        <p><?= $member['surname'] ?></p>
+                    </a>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
 </div>
